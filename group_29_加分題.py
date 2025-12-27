ffd54fd7e0b7571d14200cf834c849b7babe46a5
lilian95430 <lilian95430@gmail.com>
2025-12-27 08:33:02 +0800

    group_29_加分題.py

--- group_29_加分題.py
+++ group_29_加分題.py
@@ -10,13 +10,16 @@ from PIL import Image, ImageOps
 import pytesseract
 import matplotlib.pyplot as plt
 
+#取得使用者上傳的檔案名稱(只上傳一個)
 filename = list(uploaded.keys())[0]
 
 image = Image.open(filename)
 
+#顯示上傳的圖片，確認是否成功讀取
 plt.imshow(image)
 plt.axis('off')
 
+#同時辨識繁體中文與英文
 text = pytesseract.image_to_string(image, lang='chi_tra+eng', config='--psm 6')
 
 print("OCR 辨識結果:")
